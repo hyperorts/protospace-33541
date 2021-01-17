d0165e9cf6e5f50edf60feaede1e315ccd5d81a2
hyperorts <loss.dealer@gmail.com>
2021-01-17 18:35:07 +0900

    新規登録からdbへデータ受け渡し
    layoutのパス変更
    dbへnameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2021_01_11_150909) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
     t.text "profile", null: false
     t.text "occupation", null: false
     t.text "position", null: false
