fed8c8542dbe2ac95daa1067d80ba1257511d68c
hyperorts <loss.dealer@gmail.com>
2021-01-24 16:40:36 +0900

    prototypesコントローラーのprivateメソッド修正
    prototypesコントローラーのprivateメソッドにストロングパラメーターをセットし、特定の値のみを受け付けるようにした。且つ、user_idもmergeした

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -10,4 +10,10 @@ class PrototypesController < ApplicationController
 
   def create
   end
+
+end
+
+private
+def prototype_params
+  params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
 end
