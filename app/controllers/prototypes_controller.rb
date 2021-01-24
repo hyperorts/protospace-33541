3298f7d01c86cd385e12c1fa285373b0790f4ecb
hyperorts <loss.dealer@gmail.com>
2021-01-24 11:47:58 +0900

    重要　部分テンプレート作成
    理解が浅いので要確認
    render partialでファイルを呼び出す
    localsかcollectionは単数か複数であり、コントローラーの記述に左右される。
    コントローラー上で@prototypeか@prototypesで判断する。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,11 @@
 class PrototypesController < ApplicationController
   def index
+    # @prototypes = Prototype.all
+
   end
 
   def new
+    @prototype = Prototype.new #Tweet.new(新規オブジェクト)のnewあとで要確認
   end
 
   def create
